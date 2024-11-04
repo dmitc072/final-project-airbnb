65b2f60f062fed1f0909a7d76504986d4f3d7195
Duane Mitchell <d.mitchell@erasolutions.us>
2024-11-04 16:41:43 -0500

    Fixed to show pricing

--- src/components/approvedProperties/ApprovedforHost.js
+++ src/components/approvedProperties/ApprovedforHost.js
@@ -2,22 +2,22 @@ import { Button, Typography } from "@mui/material"
 import styles from "./approvedProperties.module.scss"
 import { collection, getDocs } from "firebase/firestore";
 import { useSelector } from "react-redux";
-import { useContext, useEffect, useState } from "react";
+import {  useEffect, useState } from "react";
 import { db } from "../../api/firebase-config";
-import { AppContext } from "../../context";
 
 export const ApprovedforHost = () => {
     const {user} = useSelector(state => state.auth)
     const [approvedProperties,setApprovedProperties] = useState([])
     const filterApprovedProperties = approvedProperties.filter(property => property !== undefined)
+    const [investment,setInvestment] =useState(0);
 
     useEffect(() => {
         const fetchPropertiesForApproval = async () => {
             try {
                 // Create a reference to the collection of properties
                 const propertySnapshot = await getDocs(collection(db, "users", user.email, "properties"));
-                console.log(propertySnapshot);
-    
+                console.log(user.email);
+                let totalInvestment = 0;
                 if (!propertySnapshot.empty) {
                     // Map through each property, fetching its ApprovedApproval documents
                     const propertyMap = await Promise.all(
@@ -26,6 +26,9 @@ export const ApprovedforHost = () => {
     
                             if (!propertyData.empty) {
                                 const availability = propertyData.docs.map((available) => {
+                                const data = available.data();
+                                console.log("data",data)
+                                totalInvestment += parseInt(data.pricePerNight, 10) || 0; // Parse and add price per night
                                     return {
                                         id: available.id,
                                         ...available.data(), // Data from ApprovedApproval documents
@@ -38,11 +41,12 @@ export const ApprovedforHost = () => {
                                     availability, // Add availability information to the property object to display becuase it is a child
                                 };
                             }
-    
+                            
                             
                         })
                     );
-                    // console.log(propertyMap)
+                   
+                    setInvestment(totalInvestment)
                     setApprovedProperties(propertyMap);
                 } else {
                    // console.log("No properties found for approval.");
@@ -67,7 +71,9 @@ export const ApprovedforHost = () => {
     return (
         <>
         <div className={styles.container}>
-            <Typography variant="h5">Approved Property Request to Host</Typography>
+            <Typography sx={{fontWeight:"bold"}} variant="h5">Approved Property Request to Host</Typography>
+            <Typography variant="h5" className={styles.margin}>Total Earnings: ${investment}</Typography>
+
             {filterApprovedProperties.map((approvedProperty, index)=>(
                 <div key={index} className={styles.approvedProperties}>
                      <div>
